6c5e4e2b7db8ae14d7a5a639df0d0006709a1145
Mark Rivera <mark.rivac@gmail.com>
2020-07-06 11:42:17 -0500

    First commit

---
+++ src/build/precache-manifest.5757c43f566f0a972e922125e0258936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f45204b9747d48dd1ad9370f635859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f53664696f8f0547450",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6db3ee304cf2b0baf371",
+    "url": "/static/js/2.7dd05f7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7dd05f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f53664696f8f0547450",
+    "url": "/static/js/main.62c7f1e5.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
